1a751e4a83cf50e9be670cde55c018572682f916
SamohTom <75379973+SamohTom@users.noreply.github.com>
2022-12-05 21:15:05 +0100

    Day3_2

--- Day3/3_2.py
+++ Day3/3_2.py
@@ -1,10 +1,25 @@
 def main():
     data = []
-    mistakes = []
+    badge = []
     final = 0
-    with open("C:/Users/thoma/Documents/PythonProjects/AdventOfCode2022/Day3/input.txt") as my_file:
+    with open("F:/Documents/CodeProjekte/PythonProjects/AdventOfCode2022/Day3/input.txt") as my_file:
         data = my_file.read().split("\n")
 
+    for i in range(0, len(data), 3):
+        bp1 = data[i]
+        bp2 = data[i+1]
+        bp3 = data[i+2]
+        for letter in set(bp1):
+            if letter in set(bp2) and letter in set(bp3):
+                badge.append(letter)
+    
+    for i in badge:
+        if i.isupper():
+            final += ord(i) - 38
+        else:
+            final += ord(i) - 96
+
+    print(final)
     
 
 
