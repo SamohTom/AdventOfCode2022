20bc3146cc04550dc183c43b359a0a9f826ba753
SamohTom <75379973+SamohTom@users.noreply.github.com>
2022-12-06 21:21:00 +0100

    Day6

---
+++ Day6/6_1.py
@@ -0,0 +1,14 @@
+def main():
+    data = ""
+    with open("Day6/input.txt") as my_file:
+        data = my_file.read()
+    
+    diffRange = 4
+
+    for i in range(len(data)-diffRange):
+        if len(set(data[i:i+diffRange])) == diffRange:
+            print(i+diffRange)
+            return
+
+if __name__ == "__main__":
+    main()
